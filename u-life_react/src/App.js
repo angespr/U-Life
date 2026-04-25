ee572a10976dab41b6eb8cafb22a4af45b33b93a
Angelina <angelinasprague41@gmail.com>
2026-04-24 19:05:25 -0700

    App.js displays navbar now correctly; Navbar pathways now align left at the top; renamed header-left to navbar-left for similar naming conventions

--- u-life_react/src/App.js
+++ u-life_react/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import Login from "./components/pages/Login";
 import Signup from "./components/pages/Signup";
+import Navbar from "./components/main parts/Navbar"
 
 function App() {
   const [page, setPage] = useState("login");
 
   return (
     <div>
+      
+      <Navbar />
+
       <button onClick={() => setPage("login")}>Login</button>
       <button onClick={() => setPage("signup")}>Signup</button>
 
@@ -15,6 +19,8 @@ function App() {
       ) : (
         <Signup />
       )}
+
+
     </div>
   );
 }
--- u-life_react/src/components/main parts/Navbar.js
+++ u-life_react/src/components/main parts/Navbar.js
@@ -5,25 +5,27 @@ import Logo from "../../assets/logo.png"
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="header-left">
+
+      <div className="navbar-left">
         <img
           src={Logo}
           alt="Logo"
           className="logo header-img"
           width="120"
         />
-      </div>        
 
-      <ul className="navbar-links">
-        <li>Finances</li>
-        <li>Productivity</li>
-        <li>Opportunities</li>
-        <li>Daily Habits</li>
-      </ul>
+        <ul className="navbar-links">
+          <li>Finances</li>
+          <li>Productivity</li>
+          <li>Opportunities</li>
+          <li>Daily Habits</li>
+        </ul>
+      </div>
 
       <button className="navbar-btn">
         Profile
       </button>
+
     </nav>
   );
 }
