1b3b2f3328a6e5134b88a14cb7427a89211a554e
Khanh Nguyen <khanh.ngu1804@gmail.com>
2026-04-25 17:33:47 -0700

    Fixed cors

--- server/server.js
+++ server/server.js
@@ -10,8 +10,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
-    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      process.env.FRONTEND_URL
+    ],
   })
 );
 
