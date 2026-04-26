5cfae513108fd660cd1bfc61287fef41ce35ae17
angespr <ange41@uw.edu>
2026-04-25 17:35:14 -0700

    updates to server

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+   origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://your-frontend-url.onrender.com"
+  ],
     credentials: true,
   })
 );
